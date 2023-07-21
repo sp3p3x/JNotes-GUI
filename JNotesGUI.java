20580db0ed6e7f98bc0e24773c56adb736eca12d
sp3p3x <594b4b@gmail.com>
2023-07-21 15:53:26 +0530

    added backbutton method

--- JNotesGUI.java
+++ JNotesGUI.java
@@ -18,14 +18,20 @@ public class JNotesGUI {
         // } catch (InterruptedException e) {
         // ;
         // }
-        // mainFrame.getContentPane().removeAll();
-        // mainFrame.revalidate();
-        // mainFrame.repaint();
+    }
+
+    void mainUI() {
+        JButton saveButton = new JButton("mainui");
+        saveButton.setBounds(230, 430, 150, 80);
+        mainFrame.add(saveButton);
+
     }
 
     void inputUI() {
 
-        JButton button = new JButton("SAVE AS NEW");
+        backButton("null", 0, 0, 100, 100);
+
+        JButton saveButton = new JButton("SAVE AS NEW");
         JTextArea input = new JTextArea(5, 20);
         JScrollPane scrollPane = new JScrollPane(input);
 
@@ -35,14 +41,41 @@ public class JNotesGUI {
         // input.setFont(new Font("Serif", Font.ITALIC, 16));
         input.setLineWrap(true);
         input.setWrapStyleWord(true);
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                reset();
+                mainUI();
+            }
+        });
 
-        button.setBounds(230, 430, 150, 80);
+        saveButton.setBounds(230, 430, 150, 80);
         input.setBounds(20, 70, 560, 300);
         scrollPane.setBounds(20, 70, 560, 300);
 
+        mainFrame.add(saveButton);
         mainFrame.add(input);
         mainFrame.add(scrollPane);
-        mainFrame.add(button);
+
+        validate();
+    }
+
+    void backButton(String text, int x, int y, int width, int height) {
+        JButton backButton = new JButton(text);
+        backButton.setBounds(x, y, width, height);
+        mainFrame.add(backButton);
+    }
+
+    // resets the current ui by removing all elements and revalidating the window
+    void reset() {
+        mainFrame.getContentPane().removeAll();
+        validate();
+    }
+
+    // to make sure all the added elements are being rendered
+    void validate() {
+        mainFrame.revalidate();
+        mainFrame.repaint();
     }
 
     public static void main(String[] args) {
